571909359c08a7d8cb53a148ad9df32bf518d14f
ReducedToBits <yashmogal308@gmail.com>
2026-09-23 23:50:42 -0700

    Ready to deploy

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 document.getElementById("login").onclick = function() {
     const ID = document.getElementById("id-user").value;
     const password = document.getElementById("user-password").value;
-    if (ID == "Yashraj" && password == "9822967122") {
+    if (ID == "Love you all" && password == "easypassword") {
         window.location.href = "dashboard.html"
+        document.getElementById("prank-audio2").play();
     } else if (ID == "" || password == "") {
         document.getElementById("wrong-cred-popup").textContent = "\u24D8 Please fill in all fields";
     } else {
