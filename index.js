4a4d69700a3957203056c60bc1bdfe7e985ae40f
ReducedToBits <yashmogal308@gmail.com>
2026-09-24 22:28:57 -0700

    code fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.getElementById("login").onclick = function() {
     const password = document.getElementById("user-password").value;
     if (ID == "SpreadLove" && password == "easypassword") {
         window.location.href = "dashboard.html"
-        document.getElementById("prank-audio2").play();
+        
     } else if (ID == "" || password == "") {
         document.getElementById("wrong-cred-popup").textContent = "\u24D8 Please fill in all fields";
     } else {
