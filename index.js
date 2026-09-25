405a49630bda8b92392c1153c6ff218122f18045
ReducedToBits <yashmogal308@gmail.com>
2026-09-24 22:33:01 -0700

    code ifx again

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ document.getElementById("login").onclick = function() {
     const password = document.getElementById("user-password").value;
     if (ID == "SpreadLove" && password == "easypassword") {
         window.location.href = "dashboard.html"
-        
     } else if (ID == "" || password == "") {
         document.getElementById("wrong-cred-popup").textContent = "\u24D8 Please fill in all fields";
     } else {
